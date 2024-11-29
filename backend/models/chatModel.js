03b650ede74c274412cc873cdd0fc8924eed6f5c
Dhruv Maheshwari <mahidhruv7@gmail.com>
2024-11-29 21:23:44 +0530

    single sided delete functionality added but chat restoration is not fixed

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -24,11 +24,25 @@ const chatModel = mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
+    deletedFor: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+        deletedAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
   },
   {
     timestamps: true,
   }
 );
+
+chatModel.index({ "deletedFor.user": 1 });
 
 const Chat = mongoose.model("Chat", chatModel);
 
--- backend/models/messageModel.js
+++ backend/models/messageModel.js
@@ -9,11 +9,18 @@ const messageModel = mongoose.Schema(
     sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     content: { type: String, trim: true },
     chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
+    readBy: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    }],
   },
   {
     timestamps: true,
   }
 );
+
+// NEW INDEX FOR BETTER QUERY PERFORMANCE
+messageModel.index({ chat: 1, createdAt: -1 });
 
 const Message = mongoose.model("Message", messageModel);
 
